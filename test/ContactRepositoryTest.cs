ba5ee9ccd2410494c06a4998bb077491abefd4c1
github_Manish <m.karn2011@gmail.com>
2020-11-21 11:50:24 +0530

    set up and seed method written

--- test/ContactRepositoryTest.cs
+++ test/ContactRepositoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using FriendZone.DAO;
 using FriendZone.Entities;
+using Microsoft.VisualBasic;
 using NUnit.Framework;
 
 namespace test
@@ -7,12 +8,17 @@ namespace test
     [TestFixture]
     public class ContactRepositoryTest
     {
-        private IContactRepository _contractRepository = new ContactRepository();
+        private IContactRepository _contractRepository;
+        [SetUp]
         public void SetUp()
         {
-
+            seedData();
         }
-
+        [TearDown]
+        public void TearDown()
+        {
+            _contractRepository = null;
+        }
         #region positive_test_cases
 
         #region test_get_methods
@@ -69,7 +75,39 @@ namespace test
 
 
         #region SeedData
-
+        public void seedData()
+        {
+            _contractRepository = new ContactRepository();
+            _contractRepository.CreateContact(new Contact()
+            {
+                ContactId = 1,
+                FirstName = "mk",
+                LastName = "karn",
+                ContactNo = "8107898901",
+                Email = "m.karn@test.com",
+                BirthDate = "01/01/1999",
+                Address = "blr",
+                City = "bangalore",
+                Pincode = "54990",
+                BloodGroup = "b+",
+                CreationDate = DateAndTime.Now.ToString()
+            });
+            _contractRepository.CreateContact(new Contact()
+            {
+                ContactId = 2,
+                FirstName = "mk",
+                LastName = "karn",
+                ContactNo = "8107898901",
+                Email = "m.karn@test.com",
+                BirthDate = "01/01/1999",
+                Address = "blr",
+                City = "bangalore",
+                Pincode = "54990",
+                BloodGroup = "b+",
+                CreationDate = DateAndTime.Now.ToString()
+            });
+           
+        }
 
         #endregion
 
--- test/ContactServiceTest.cs
+++ test/ContactServiceTest.cs
@@ -1,11 +1,39 @@
-﻿namespace test
+﻿using FriendZone.DAO;
+using FriendZone.Entities;
+using FriendZone.Service;
+using Microsoft.VisualBasic;
+using NUnit.Framework;
+
+namespace test
 {
     public class ContactServiceTest
     {
+        private IContactRepository _contractRepository;
+        private IContactService _contractService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            seedData();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _contractRepository = null;
+            _contractService = null;
+        }
         #region positive_test_cases
 
         #region test_get_methods
+        [TestCase]
+        public void GivenValidContractToContractServiceReturnCreatedContract()
+        {
+            var contract = new Contact() { Address = "abc" };
 
+            var createdContract = _contractService.AddContact(contract);
+
+            Assert.AreEqual(contract.Address, createdContract.Address);
+        }
         #endregion
 
         #region test_manipulation_methods
@@ -31,6 +59,40 @@
         #endregion
 
         #region SeedData
+        public void seedData()
+        {
+            _contractRepository = new ContactRepository();
+            _contractRepository.CreateContact(new Contact()
+            {
+                ContactId = 1,
+                FirstName = "mk",
+                LastName = "karn",
+                ContactNo = "8107898901",
+                Email = "m.karn@test.com",
+                BirthDate = "01/01/1999",
+                Address = "blr",
+                City = "bangalore",
+                Pincode = "54990",
+                BloodGroup = "b+",
+                CreationDate = DateAndTime.Now.ToString()
+            });
+            _contractRepository.CreateContact(new Contact()
+            {
+                ContactId = 2,
+                FirstName = "mk",
+                LastName = "karn",
+                ContactNo = "8107898901",
+                Email = "m.karn@test.com",
+                BirthDate = "01/01/1999",
+                Address = "blr",
+                City = "bangalore",
+                Pincode = "54990",
+                BloodGroup = "b+",
+                CreationDate = DateAndTime.Now.ToString()
+            });
+            _contractService = new ContactService(_contractRepository);
+
+        }
 
 
         #endregion
